7b9f764a1844cb8b7edfd97b2b671ade8a1609f4
Daniel Goodman <dgoodman1@gmail.com>
2024-02-02 11:43:27 -0500

    beefing up objects; placeholder

--- are_you_the_one.py
+++ are_you_the_one.py
@@ -1,3 +1,7 @@
+import itertools as it
+from dataclasses import dataclass
+from math import factorial
+from typing import Any
 import pandas as pd
 import numpy as np
 
@@ -5,27 +9,75 @@ import numpy as np
 * Can either submit final matchups ahead of time or generate them randomly.
 * Retain history.
 * Starting with a "brute force" method of enumerating all possibilities and
-    then eliminating paths false paths as they are identified.
+    then eliminating false paths as they are identified.
 * Need to go back and forth between individual matchups and group info.
 """
 
 
+@dataclass
 class Contestant:
     """A single person."""
-    pass
+    name: str
+    sex: str
+    gender_preference: str
 
+    def __post_init__(self):
+        assert self.sex in ('Male', 'Female',)
+        assert self.gender_preference in ('Male', 'Female', 'Both',)
 
+@dataclass
 class Guy(Contestant):
     """A single guy contestant."""
-    def __init__(self, name):
-        self.name = name
-        self.sex = 'M'
+    def __init__(self, name: str, gender_preference: str = 'Female'):
+        super().__init__(name, 'Male', gender_preference=gender_preference)
 
-
+@dataclass
 class Girl(Contestant):
-    def __init__(self, name):
-        self.name = name
-        self.sex = 'F'
+    def __init__(self, name: str, gender_preference: str = 'Male'):
+        super().__init__(name, 'Female', gender_preference=gender_preference)
+
+
+class Match:
+    def __init__(self, guy: Guy, girl: Girl):
+        assert guy.gender_preference == girl.sex and \
+            girl.gender_preference == guy.sex
+        self.guy = guy
+        self.girl = girl
+
+    def __repr__(self):
+        print(f"({guy}, {girl})")
+
+
+class Path:
+    """A single path through the tournament."""
+    def __init__(self, matches: set[tuple]):
+        self._matches = matches
+        self._N = len(matches)
+
+    def __len__(self):
+        return len(self.matches)
+
+    def __repr__(self):
+        return f'Path({self.matches})'
+
+    def __str__(self):
+        return f'Path({self.matches})'
+
+    def __iter__(self):
+        return iter(self.matches)
+
+    @property
+    def matches(self):
+        return self._matches
+
+    @matches.setter
+    def matches(self, matches):
+        self._matches = matches
+
+
+class ActivePaths:
+    def __init__(self, paths):
+        self.paths = paths
 
 
 class Grid:
@@ -40,6 +92,70 @@ class Grid:
 
         self.X = pd.DataFrame(data, index=[guy.name for guy in guys],
                               columns=[girl.name for girl in girls])
+
+
+class Matrix(np.ndarray):
+    """
+    Numpy array of all possible matchups. Guys are represented by the
+    column index and their corresponding match is represented by the value.
+    """
+    def __new__(cls, N_matchups):
+        obj = np.zeros((factorial(N_matchups), N_matchups),
+                       dtype=np.int8).view(cls)
+        obj.N_matchups = N_matchups
+        obj._matrix = obj._create_matrix()
+        return obj
+
+    def _create_matrix(self, permutation_fcn=it.permutations):
+        """Create a matrix of all possible matchups."""
+        perm_it = permutation_fcn(np.arange(self.N_matchups))
+        return np.array(tuple(perm_it), dtype=np.int8)
+
+    def drop_path(self, path: Path):
+        """Drop path (row) from the matrix."""
+        mask = np.equal(self._matrix, path).all(axis=1)
+        if mask.sum() > 0:
+            self._matrix = self._matrix[~mask]
+
+    def drop_paths_containing_match(self, match: Match):
+        """
+        Drop paths containing a given match.
+
+        After a truth booth tells us that a match is not correct, we
+        can drop all paths containing that match.
+        """
+        mask = self._matrix[:, match[0]] == match[1]
+        if mask.sum() > 0:
+            self._matrix = self._matrix[~mask]
+            print(f"Dropped {mask.sum():,.0f} paths.")
+
+    def drop_paths_not_containing_match(self, match: Match):
+        """
+        Drop paths that do not contain a given match.
+
+        After a truth booth tells us that a match is correct, we
+        can drop all paths that do not contain that match.
+        """
+        mask = self._matrix[:, match[0]] == match[1]
+        if mask.sum() > 0:
+            self._matrix = self._matrix[mask]
+            print(f"Dropped {mask.sum():,.0f} paths.")
+
+    def drop_paths_not_containing_n_matches(self, path: Path, n: int):
+        """
+        Drop paths that don't contain n matches from a given path.
+
+        After a ceremony, we know that n matches are correct.
+        """
+        assert n <= len(path)
+        eq_mask = np.equal(self._matrix, path)
+        mask = eq_mask.sum(axis=1) == n
+        if mask.sum() > 0:
+            self._matrix = self._matrix[~mask]
+
+    def __getitem__(self, indices):
+        return self._matrix[indices]
+
 
 
 class Round:
